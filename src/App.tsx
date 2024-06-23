97ecf49d3d439a0a17143309c99c45d835894279
saaasuika <tokage3geko653@gmail.com>
2024-06-23 15:21:55 +0900

    add 3days forecast

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,18 @@ import Loading from "./components/Loading"
 type ResultsState = {  
     country: string
     cityName: string
-    temperature: string
-    conditionText: string 
-    icon: string
+    temperature1: string
+    temperature2: string
+    temperature3: string
+    conditionText1: string 
+    conditionText2: string 
+    conditionText3: string 
+    icon1: string
+    icon2: string
+    icon3: string
+    date1: string
+    date2: string
+    date3: string
 }  
 
 const App = () => {
@@ -18,23 +27,42 @@ const App = () => {
     const [results, setResults] = useState<ResultsState>({
         country: "",
         cityName: "",
-        temperature: "", 
-        conditionText: "",
-        icon: ""
+        temperature1: "", 
+        temperature2: "", 
+        temperature3: "", 
+        conditionText1: "",
+        conditionText2: "",
+        conditionText3: "",
+        icon1: "",
+        icon2: "",
+        icon3: "",
+        date1: "",
+        date2: "",
+        date3: "",
     })
 
     const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault() 
         setLoading(true)
-    fetch(`https://proxy-server-rust-five.vercel.app/weather-data?${city}`)
+        fetch(`https://proxy-server-eta-azure.vercel.app/weather-data?${city}`)
             .then(res=>res.json())
             .then(data => {
+                console.log(data); 
                 setResults({
                     country: data.location.country,
                     cityName: data.location.name,
-                    temperature: data.current.temp_c,
-                    conditionText: data.current.condition.text,
-                    icon: data.current.condition.icon
+                    temperature1: data.forecast.forecastday[0].day.avgtemp_c,
+                    temperature2: data.forecast.forecastday[1].day.avgtemp_c,
+                    temperature3: data.forecast.forecastday[2].day.avgtemp_c,
+                    conditionText1: data.forecast.forecastday[0].day.condition.text,
+                    conditionText2: data.forecast.forecastday[1].day.condition.text,
+                    conditionText3: data.forecast.forecastday[2].day.condition.text,
+                    icon1: data.forecast.forecastday[0].day.condition.icon,
+                    icon2: data.forecast.forecastday[1].day.condition.icon,
+                    icon3: data.forecast.forecastday[2].day.condition.icon,
+                    date1: data.forecast.forecastday[0].date,
+                    date2: data.forecast.forecastday[1].date,
+                    date3: data.forecast.forecastday[2].date,
                 })
                 setLoading(false)
                 setCity("")
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,9 +2,18 @@ type ResultsProps = {
     results: { 
         country: string
         cityName: string
-        temperature: string
-        conditionText: string 
-        icon: string
+        temperature1: string
+        temperature2: string
+        temperature3: string
+        conditionText1: string 
+        conditionText2: string 
+        conditionText3: string 
+        icon1: string
+        icon2: string
+        icon3: string
+        date1: string
+        date2: string
+        date3: string
     }
 }    
 
@@ -13,13 +22,30 @@ const Results = (props: ResultsProps) => {
         <div>          
             {props.results.country && 
                 <div>
+                    
                     <div className="results-country">{props.results.country}</div>
                     <div className="results-city">{props.results.cityName}</div>
-                    <div className="results-temp">{props.results.temperature} <span>°C</span></div>
-                    <div className="results-condition">
-                        <img src={props.results.icon} alt="icon"/>
-                        <span>{props.results.conditionText}</span>
+                    <div className="results-dates-container">
+                        <div className="results-date-group">
+                            <div className="results-date">{props.results.date1}</div>
+                            <div className="results-temp">{props.results.temperature1} <span>°C</span></div>
+                            <img src={props.results.icon1} alt="icon"/>
+                            <span>{props.results.conditionText1}</span>
+                        </div>
+                        <div className="results-date-group">
+                            <div className="results-date">{props.results.date2}</div>
+                            <div className="results-temp">{props.results.temperature2} <span>°C</span></div>
+                            <img src={props.results.icon2} alt="icon"/>
+                            <span>{props.results.conditionText2}</span>
+                        </div>
+                        <div className="results-date-group">
+                            <div className="results-date">{props.results.date3}</div>
+                            <div className="results-temp">{props.results.temperature3} <span>°C</span></div>
+                            <img src={props.results.icon3} alt="icon"/>
+                            <span>{props.results.conditionText3}</span>
+                        </div>
                     </div>
+                    
                 </div>
             }
         </div>
