441f0c61041c01575dc20b51a893cc81968e3371
saaasuika <tokage3geko653@gmail.com>
2024-06-23 19:21:15 +0900

    style: border separating the days

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -29,14 +29,14 @@ const Results = (props: ResultsProps) => {
                     <div className="results-country">{props.results.country}</div>
                     <div className="results-city">{props.results.cityName}</div>
                     <div className="results-dates-container">
-                        <div className="results-date-group">
+                        <div className="results-date-group-border">
                             <div className="results-date">{props.results.date1}</div>
                             <div className="results-maxtemp">{props.results.max_temperature1}<span>/</span></div>
                             <div className="results-mintemp">{props.results.min_temperature1}<span>°C</span></div>
                             <img src={props.results.icon1} alt="icon"/>
                             <span>{props.results.conditionText1}</span>
                         </div>
-                        <div className="results-date-group">
+                        <div className="results-date-group-border">
                             <div className="results-date">{props.results.date2}</div>
                             <div className="results-maxtemp">{props.results.max_temperature1}<span>/</span></div>
                             <div className="results-mintemp">{props.results.min_temperature1} <span>°C</span></div>
